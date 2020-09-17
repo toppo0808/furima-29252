670f319a024e751c6903262f3c35e6833894591b
toppo0808 <jnkota0808@gmail.com>
2020-09-17 18:23:59 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
     validates :nickname
     validates :email
     validates :password, {/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}$/i}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name, format:   { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :last_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    FURIGANA_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :first_name, format: { with: NAME_REGEX, message: "is invalid. Input full-width characters."}
+    validates :last_name, format:   { with: NAME_REGEX, message: "is invalid. Input full-width characters."}
+    validates :first_furigana, format: { with: FURIGANA_REGEX, message: "is invalid. Input full-width katakana characters."}
+    validates :last_furigana, format: { with: FURIGANA_REGEX, message: "is invalid. Input full-width katakana characters."}
     validates :birthday
   end
 
